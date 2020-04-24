ccfa241439eca8b817c3749a91bb4081876e0ef0
Dominique Miller <dominique.p.miller@gmail.com>
2020-04-24 13:26:43 -0400

    Project Colors

---
+++ JobPostings/Extensions/ColorExtension.swift
@@ -0,0 +1,18 @@
+//
+//  ColorExtension.swift
+//  JobPostings
+//
+//  Created by Dominique Miller on 4/23/20.
+//  Copyright © 2020 DomsBiz. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color {
+    enum JobPostings {
+        static let azure = Color(red: 0 / 255.0, green: 174 / 255.0, blue: 239 / 255.0)
+        static let barney = Color(red: 198.0 / 255.0, green: 39.0 / 255.0, blue: 177.0 / 255.0)
+        static let gray  = Color(red: 216 / 255.0, green: 216 / 255.0, blue: 216 / 255.0)
+        static let yellow = Color(red: 184 / 255.0, green: 214 / 255.0, blue: 55 / 255.0)
+    }
+}
